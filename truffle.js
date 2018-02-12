8c9a5ea2748c26f7cf36142a6feb9c793744af16
david barrar <davidbarrar@davids-MacBook-Pro.local>
2018-02-11 20:48:55 -1000

    deployed to rinkeby

--- truffle.js
+++ truffle.js
@@ -6,14 +6,13 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xb20bc63fb2ecaf381f065a94f53b2071ab651947", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
     }
-    // ,
-    // rinkeby: {
-    //   host: "localhost", // Connect to geth on the specified
-    //   port: 8545,
-    //   from: "0x038343bFaf1F35B01D91513C8472764D55474045", // default address to use for any transaction Truffle makes during migrations
-    //   network_id: 4,
-    //   gas: 4612388 // Gas limit used for deploys
-    // }
   }
 };
